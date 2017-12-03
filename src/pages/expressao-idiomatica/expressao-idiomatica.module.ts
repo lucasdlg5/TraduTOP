e5d386fbff45dd6aa5b3a31ea2ab6a2fd7619642
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2017-12-03 18:47:55 -0200

    Criado base pagina Expressao_Idiomatica

---
+++ src/pages/expressao-idiomatica/expressao-idiomatica.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ExpressaoIdiomaticaPage } from './expressao-idiomatica';
+
+@NgModule({
+  declarations: [
+    ExpressaoIdiomaticaPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ExpressaoIdiomaticaPage),
+  ],
+})
+export class ExpressaoIdiomaticaPageModule {}
