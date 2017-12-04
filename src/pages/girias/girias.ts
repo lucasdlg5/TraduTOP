6fd42f4ccf702868e4be9913a153312835cc53f0
Lucas Domingos Leão Gomes <lucasdomingosleao55@hotmail.com>
2017-12-04 02:09:20 -0200

    Configuração de card dinamico Girias

--- src/pages/girias/girias.ts
+++ src/pages/girias/girias.ts
@@ -7,6 +7,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+//vc executou ai? aff vei, q chato isso, reinicia o server ai HAUHUAHAUHAUHAUAHU. memo erro?//o que? vc colocou o terminal? apareceu n kkkk
+
 
 @IonicPage()
 @Component({
@@ -14,8 +16,44 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'girias.html',
 })
 export class GiriasPage {
+  giriasItems: any[];
+
+
+
+
+
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+
+  /*  let girias = [
+      {
+    	//"giria": {
+    		"id_giria": "1",
+    		"titulo": "All mouth and no trousers",
+    		"traducaoTitulo": "Toda a boca e sem calças.",
+    		"descricao": "Todas as conversas, sem ação.",
+    		"exemplo": "Todas as Dont listen to him. Hes all mouth and no trousers, sem ação.",
+    		"exemploTraducao": "Não o escute. Ele é toda boca e sem calças.",
+    		"dificuldade": "2"
+    	//}
+    }
+  ];*/
+
+  this.giriasItems = [
+      {
+    	giria: {
+    		id_giria: "1",
+    		titulo: "All mouth and no trousers",
+    		traducaoTitulo: "Toda a boca e sem calças.",
+    		descricao: "Todas as conversas, sem ação.",
+    		exemplo: "Todas as Dont listen to him. Hes all mouth and no trousers, sem ação.",
+    		exemploTraducao: "Não o escute. Ele é toda boca e sem calças.",
+    		dificuldade: "2"
+    	}
+    }
+  ];
+
   }
 
   goGirias(): void {
@@ -25,5 +63,7 @@ this.navCtrl.push("GiriasPage");
   ionViewDidLoad() {
     console.log('ionViewDidLoad GiriasPage');
   }
+
+
 
 }
